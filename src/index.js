ab7042497e7bb683de2e582ac88bc12bb7a38d9b
Matt Draghi <mtd5264@gmail.com>
2022-02-12 21:56:20 -0500

    commit tree 6873c01992f24929eacce7f677d0e42e0331eacd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//step 1. import react-router functions
+import { BrowserRouter, Route, Switch , NavLink} from "react-router-dom"
+
+/*Add basic styling for NavLinks*/
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+}
+
+// Define the Navbar componenet
+function NavBar(){
+  return(
+    <div>
+      <NavLink
+        to="/"
+        /*set exact so it knows to only set activeStyle when route is deeply equal to link*/
+        exact
+        style={linkStyles}
+        //add prop for activeStyle
+        activeStyle={{background: "darkblue",}}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
+    </div>
+  )
+}
 
 function Home() {
   return (
@@ -9,8 +60,67 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+//Step 2. Use <Route> components to define client-side routes in our app
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+//Step 3. Use <BrowserRouter> component to wrap the entire application and 
+//provide React Router context features
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
